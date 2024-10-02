860dea80b1c4e0f18b022808e5b634312d1b3dad
madmax <maksimkholin@gmail.com>
2024-10-02 12:41:13 +0300

    fix Main

--- src/main/kotlin/ui/Main.kt
+++ src/main/kotlin/ui/Main.kt
@@ -6,14 +6,19 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import resources.MyStrings
-import ui.screens.ScreenChooseMode
-import ui.screens.ScreenWordLength
+import ui.screens.Screen1WordLength
+import ui.screens.Screen2ChooseMode
+import ui.screens.Screen3SetMode
+import ui.screens.Screen4ExludeMode
 
 
 @Composable
 fun App() {
-//    ScreenWordLength()
-    ScreenChooseMode()
+    Screen1WordLength()
+//    Screen2ChooseMode()
+//    Screen3SetMode()
+//    Screen4ExludeMode()
+
 }
 
 fun main() = application {
