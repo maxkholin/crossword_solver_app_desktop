3bbfcdaa38161d9c249b6b4c93480b5c0cbbb006
madmax <maksimkholin@gmail.com>
2024-10-08 01:01:41 +0300

    изменил 5 экран

--- src/main/kotlin/ui/screens/Screen5Result.kt
+++ src/main/kotlin/ui/screens/Screen5Result.kt
@@ -5,29 +5,42 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import logic.getListOfWords
+import data.LetterButtonModel
+import logic.searchBySet
 import resources.MyStrings
+import ui.MODE_SET
 
 @Composable
-fun Screen5Result() {
-//    val words = getListOfWords(5)
-    val words = mutableListOf<String>()
-
-    if (words.isEmpty()) {
-        TryAgain()
+fun Screen5Result(
+    wordLength: Int,
+    words: List<String>,
+    mode: Int,
+    chosenLetters: Set<LetterButtonModel>,
+    exludeLetters: Set<LetterButtonModel>
+) {
+    val foundWords = if (mode == MODE_SET) {
+        searchBySet(words, chosenLetters, exludeLetters)
     } else {
-        Result(words)
+        listOf("Привет", " Ромашки")
+    }
+
+    if (foundWords.isEmpty()) {
+        TryAgain(wordLength, chosenLetters, exludeLetters)
+    } else {
+        Result(foundWords)
     }
 }
 
+
 @Composable
 private fun Result(words: List<String>) {
-    Column(modifier = Modifier.fillMaxSize(),
+    Column(
+        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
@@ -48,7 +61,11 @@ private fun Result(words: List<String>) {
 }
 
 @Composable
-private fun TryAgain() {
+private fun TryAgain(
+    wordLength: Int,
+    chosenLetters: Set<LetterButtonModel>,
+    exludeLetters: Set<LetterButtonModel>
+) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -59,11 +76,20 @@ private fun TryAgain() {
             fontSize = 48.sp
         )
         Spacer(modifier = Modifier.height(48.dp))
-        Button(onClick = {}) {
+        Button(onClick = {
+//            App()
+        }) {
             Text(
                 text = MyStrings.TRY_AGAIN,
                 fontSize = 36.sp
             )
         }
+        Spacer(modifier = Modifier.height(48.dp))
+        Text(
+            text = "length = $wordLength, " +
+                    "choose letters = ${chosenLetters.joinToString(" ")}" +
+                    "exlude letters = ${exludeLetters.joinToString(" ")}",
+            fontSize = 24.sp
+        )
     }
 }
