1c2652b8f3b1f37a3438230eb4d946679280aec7
madmax <maksimkholin@gmail.com>
2024-10-16 00:37:36 +0300

    update model

--- src/main/kotlin/data/LetterButtonModel.kt
+++ src/main/kotlin/data/LetterButtonModel.kt
@@ -2,28 +2,10 @@ package data
 
 data class LetterButtonModel(
     val letter: Char,
-    var isPressed: Boolean,
-    val type: HowCommonLetterType
+    var isPressed: Boolean
 )
 
-enum class HowCommonLetterType {
-    MOST_COMMON,
-    USUALLY,
-    RARE,
-}
 
 fun createLetters(): List<LetterButtonModel> {
-    return createLetterModels(createMostCommonLetters(), HowCommonLetterType.MOST_COMMON) +
-            createLetterModels(createUsuallyLetters(), HowCommonLetterType.USUALLY) +
-            createLetterModels(createRareLetters(), HowCommonLetterType.RARE)
+    return ('а'..'я').filter { it != 'ё' }.map { LetterButtonModel(it, isPressed = false) }
 }
-
-private fun createLetterModels(letters: List<Char>, type: HowCommonLetterType): List<LetterButtonModel> {
-    return letters.map { LetterButtonModel(it, isPressed = false, type) }
-}
-
-fun createMostCommonLetters(): List<Char> = listOf('о', 'а', 'и', 'е', 'т', 'н', 'р', 'с')
-
-fun createUsuallyLetters(): List<Char> = listOf('к', 'л', 'в', 'ь', 'п', 'д', 'м', 'й', 'у', 'ы', 'з')
-
-fun createRareLetters(): List<Char> = listOf('я', 'б', 'г', 'ч', 'ц', 'ш', 'ж', 'х', 'ф', 'щ', 'ю', 'э', 'ъ')
