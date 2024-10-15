b62b6835c4ba7e14e1612783c4d82b461c89103c
madmax <maksimkholin@gmail.com>
2024-10-16 00:43:21 +0300

    update screen2

--- src/main/kotlin/ui/screens/Screen2ChooseMode.kt
+++ src/main/kotlin/ui/screens/Screen2ChooseMode.kt
@@ -1,16 +1,14 @@
 package ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,92 +17,87 @@ import ui.MODE_MASK
 import ui.MODE_SET
 
 @Composable
-fun Screen2ChooseMode(
-    onButtonModeClick: (Int) -> Unit,
-    mode: Int
-) {
-    Screen2Display(
-        onButtonModeClick,
-        mode
-    )
-}
-
-@Composable
-private fun Screen2Display(
-    onButtonModeClick: (Int) -> Unit,
-    mode: Int
+fun ScreenChooseMode(
+    onModeButtonClick: (Int) -> Unit
 ) {
     Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.White)
-                .padding(10.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = MyStrings.CHOOSE_MODE,
-                textAlign = TextAlign.Center,
-                fontSize = 48.sp
-            )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 48.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                DisplayButtons(onButtonModeClick)
-            }
-
-            DisplayChosenMode(mode)
-        }
+        Text(
+            text = MyStrings.CHOOSE_MODE,
+            textAlign = TextAlign.Center,
+            fontSize = 48.sp
+        )
+        Spacer(Modifier.height(36.dp))
+        DisplayButtons(onModeButtonClick)
+        DisplayDescription()
     }
 }
 
 @Composable
 private fun DisplayButtons(
-    onButtonModeClick: (Int) -> Unit
+    onModeButtonClick: (Int) -> Unit
 ) {
-    for (i in 1..2) {
-        Column(
-            modifier = Modifier
-                .padding(top = 64.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 48.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        for (i in 1..2) {
             val modeButton = if (i == MODE_SET) MODE_SET else MODE_MASK
             Button(
-                onClick = { onButtonModeClick(modeButton) },
+                onClick = { onModeButtonClick(modeButton) },
                 modifier = Modifier
-                    .padding(36.dp)
-                    .size(160.dp)
+                    .padding(16.dp)
+                    .size(200.dp)
                     .clip(CircleShape),
-                elevation = ButtonDefaults.elevation(pressedElevation = 64.dp),
+                elevation = ButtonDefaults.elevation(defaultElevation = 20.dp),
             ) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = MyStrings.I_AM_BUTTON)
-                    Text(text = MyStrings.CLICK_ME)
+                val modeText = if (i == MODE_SET) {
+                    MyStrings.CHOOSE_MODE_SET
+                } else {
+                    MyStrings.CHOOSE_MODE_MASK
                 }
+                Text(
+                    text = modeText,
+                    fontSize = 24.sp,
+                    lineHeight = 30.sp,
+                    textAlign = TextAlign.Center
+                )
             }
-
-            val modeText = if (i == MODE_SET) MyStrings.CHOOSE_MODE_SET else MyStrings.CHOOSE_MODE_MASK
-            Text(
-                text = modeText,
-                fontSize = 24.sp
-            )
         }
     }
 }
 
 @Composable
-private fun DisplayChosenMode(mode: Int) {
-    val modeString = if (mode == MODE_SET) MyStrings.CHOOSE_MODE_SET else MyStrings.CHOOSE_MODE_MASK
-    Text(
-        text = "Вы выбрали режим: $modeString",
-        modifier = Modifier
-            .padding(12.dp)
-    )
+fun DisplayDescription() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        for (i in 1..2) {
+            val modeText = if (i == MODE_SET) {
+                MyStrings.MODE_SET_DESCRIPTION
+            } else {
+                MyStrings.MODE_MASK_DESCRIPTION
+            }
+            Text(
+                text = modeText,
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                lineHeight = 30.sp,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp)
+            )
+        }
+    }
 }
+
+
+
