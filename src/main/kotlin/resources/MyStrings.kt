76c9c56b8c3891c8ddff4890b15631128341dfb0
madmax <maksimkholin@gmail.com>
2024-10-16 00:43:55 +0300

    update string res

--- src/main/kotlin/resources/MyStrings.kt
+++ src/main/kotlin/resources/MyStrings.kt
@@ -3,42 +3,26 @@ package resources
 object MyStrings {
     const val APP_TITLE = "Crossword Solver by Max Kholin"
 
-    const val PARSE_LENGTH = "Введите длину искомого слова:"
-    const val PARSE_LENGTH_SECOND = "Выберите количество букв в слове"
-
-    const val LENGTH_REMINDER = "Напоминание: Длина выбранного слова - "
-
-    const val LABEL_PARSE_LENGTH = "Число от 3 до 18"
+    const val CHOOSE_LENGTH = "Выберите количество букв в слове"
 
     const val NEXT = "Далее"
-
-    const val TOO_SHORT_OR_LONG = "Извините слова короче 3 или длиннее 18 букв мы не ищем.\n" +
-            "Попробуйте снова"
-
-    const val NOT_A_NUMBER = "Вы ввели не число, попробуйте снова"
-
 
     const val CHOOSE_MODE = "Выберите режим поиска"
     const val CHOOSE_MODE_SET = "По множеству букв"
     const val CHOOSE_MODE_MASK = "По маске"
 
-    const val I_AM_BUTTON = "Я кнопка :)"
-    const val CLICK_ME = "Нажми меня!"
+    const val MODE_SET_DESCRIPTION = "Идеально для таких игр, как \"Словли\" и \"5 букв\""
+    const val MODE_MASK_DESCRIPTION = "Идеально для кроссвордов и сканвордов"
 
     const val MODE_SET = "Режим: поиск по множеству букв"
-
-    const val MODE_MASK = "Режим: поиск по маске" +
-            "Введите строку в формате \\\"_____\\\" вместо неизвестных букв указывая '*', " +
-            "например \\\"а**уз\\\":"
-
     const val MODE_EXLUDE_LETTERS = "Режим: исключение букв"
+    const val MODE_MASK = "Режим: поиск по маске"
 
     const val CHOOSE_LETTERS = "Отметьте нужные буквы"
+    const val SEARCH = "Искать"
 
     const val RESULT = "Найдено слов: "
-
     const val NOT_FOUND = "Ничего не найдено"
-
     const val TRY_AGAIN = "Попробовать снова"
 
 }
