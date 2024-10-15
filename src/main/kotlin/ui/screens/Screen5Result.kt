1e166516c2f872e33160e37a70098698f417a0b8
madmax <maksimkholin@gmail.com>
2024-10-16 00:53:39 +0300

    update screen5

--- src/main/kotlin/ui/screens/Screen5Result.kt
+++ src/main/kotlin/ui/screens/Screen5Result.kt
@@ -1,8 +1,9 @@
 package ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,36 +12,46 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import data.LetterButtonModel
+import logic.searchByMask
 import logic.searchBySet
 import resources.MyStrings
 import ui.MODE_SET
 
 @Composable
-fun Screen5Result(
+fun ScreenResult(
     words: List<String>,
     mode: Int,
+    countOfLetters: Int,
     chosenLetters: Set<LetterButtonModel>,
     exludeLetters: Set<LetterButtonModel>,
+    maskWord: String,
     onTryAgainButtonClick: () -> Unit
 ) {
     val foundWords = if (mode == MODE_SET) {
         searchBySet(words, chosenLetters, exludeLetters)
     } else {
-        listOf("Привет", " Ромашки")
+        searchByMask(words, maskWord)
     }
 
     if (foundWords.isEmpty()) {
         TryAgain(onTryAgainButtonClick)
     } else {
-        Result(foundWords, onTryAgainButtonClick)
+        Result(foundWords, countOfLetters, onTryAgainButtonClick)
     }
 }
 
 @Composable
 private fun Result(
     words: List<String>,
+    countOfLetters: Int,
     onTryAgainButtonClick: () -> Unit
 ) {
+    val gridColumns = if (countOfLetters <= 7) {
+        3
+    } else {
+        2
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -50,10 +61,12 @@ private fun Result(
             fontSize = 48.sp,
             modifier = Modifier.padding(horizontal = 100.dp, vertical = 32.dp)
         )
-        LazyColumn(
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(gridColumns),
             modifier = Modifier
                 .weight(1f)
-                .padding(bottom = 16.dp)
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             items(words) { word ->
                 Text(
@@ -88,8 +101,10 @@ private fun TryAgain(
 
 @Composable
 fun TryAgainButton(onTryAgainButtonClick: () -> Unit) {
-    Button(onClick = onTryAgainButtonClick,
-        modifier = Modifier.padding(16.dp)) {
+    Button(
+        onClick = onTryAgainButtonClick,
+        modifier = Modifier.padding(16.dp)
+    ) {
         Text(
             text = MyStrings.TRY_AGAIN,
             fontSize = 36.sp
